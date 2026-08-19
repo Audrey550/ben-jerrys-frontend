10602623cfa94f4f816e37509f9bc74849fdf1da
Audrey <r0982547@student.thomasmore.be>
2026-08-19 23:13:19 +0200

    feat: add order status management

--- src/Admin.vue
+++ src/Admin.vue
@@ -10,6 +10,25 @@ async function fetchOrders() {
     orders.value = data.orders
 }
 
+async function updateStatus(order, newStatus) {
+    const response = await fetch(
+        `http://localhost:3000/api/orders/${order.id}`,
+        {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                status: newStatus
+            })
+        }
+    )
+
+    const updatedOrder = await response.json()
+
+    order.status = updatedOrder.status
+}
+
 onMounted(() => {
     fetchOrders()
 })
@@ -22,19 +41,35 @@ onMounted(() => {
         <h2>Orders</h2>
 
         <div
-            v-for="order in orders"
-            :key="order._id"
-            class="admin-order"
-        >
-            <div>
-                <strong>Order #{{ order.id }}</strong>
-                <p>{{ order.customer.name }}</p>
-            </div>
+    v-for="order in orders"
+    :key="order._id"
+    class="admin-order"
+>
+    <div>
+        <strong>Order #{{ order.id }}</strong>
+        <p>{{ order.customer.name }}</p>
+    </div>
 
-            <div>
-                <strong>€{{ order.totalPrice.toFixed(2) }}</strong>
-                <p>{{ order.status }}</p>
-            </div>
-        </div>
+    <div>
+        <strong>€{{ order.totalPrice.toFixed(2) }}</strong>
+
+        <select
+            :value="order.status"
+            @change="updateStatus(order, $event.target.value)"
+        >
+            <option value="to_process">
+                To process
+            </option>
+
+            <option value="shipped">
+                Shipped
+            </option>
+
+            <option value="cancelled">
+                Cancelled
+            </option>
+        </select>
+    </div>
+</div>
     </main>
 </template>
